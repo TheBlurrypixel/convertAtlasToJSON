62212b091df2fe49d1d3a85360a1ec6998910008
TheBlurrypixel <mgochoco@yahoo.com>
2019-01-08 15:45:45 -0800

    Added read file dialog

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const prompt = require('electron-prompt');
 var fs = require('fs')
 
 function start() {
-  dialog.showOpenDialog({filters: [ {name: 'atlas', extensions: ['atlas']}, {name: 'All Files', extensions: ['*']} ] });
+  var files = dialog.showOpenDialog({filters: [ {name: 'atlas', extensions: ['atlas']}, {name: 'All Files', extensions: ['*']} ] });
+
+	if((files) && (files.length > 0)) {
+    console.log(fs.readFileSync(files[0], 'utf8'));
+  }
 }
 
 app.on('window-all-closed', e => e.preventDefault() );
