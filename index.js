5fe45ea12be7e71bf2c2c0b9792f57136eaabbd7
TheBlurrypixel <mgochoco@yahoo.com>
2019-01-08 10:30:00 -0800

    adding index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const {app, dialog, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+const prompt = require('electron-prompt');
+
+var fs = require('fs')
