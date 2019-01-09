88b0e746c7f4149ec454a34ba691fa5fc656dbc4
TheBlurrypixel <mgochoco@yahoo.com>
2019-01-08 17:04:59 -0800

    Adding preliminary string manip for json

--- index.js
+++ index.js
@@ -4,13 +4,39 @@ const url = require('url');
 const prompt = require('electron-prompt');
 
 var fs = require('fs')
+var re = /[^\:]*[^d]\.[^d][^\:]*/;
+var re2 = /^[^\:]*$/;
 
 function start() {
   var files = dialog.showOpenDialog({filters: [ {name: 'atlas', extensions: ['atlas']}, {name: 'All Files', extensions: ['*']} ] });
 
+  var res = {};
+
 	if((files) && (files.length > 0)) {
-    console.log(fs.readFileSync(files[0], 'utf8'));
+    var atlasText = fs.readFileSync(files[0], 'utf8');
+    var tokens = atlasText.trim().split('\n');
+    tokens.forEach((i) => {
+      if(re.test(i)) {
+        // console.log('i is: ' + i);
+        let solves = i.match(/[^\.]+/g);
+        res['name'] = solves.input ? solves : solves.reduce( (acc, cur) => {
+          return acc + '_' + cur;
+        });
+      }
+      else if(re2.test(i)) {
+        console.log(i + ' going up a level');
+      }
+      else {
+        var propSplits = i.split(':', 2);
+        var resSplits = propSplits.map( (item) => {
+          return item.trim();
+        });
+        res[resSplits[0]] = resSplits[1];
+      }
+    });
+    console.log(res);
   }
+  app.quit();
 }
 
 app.on('window-all-closed', e => e.preventDefault() );
