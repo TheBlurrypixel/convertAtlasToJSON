1dbe85837284f881d5584190270f0a817ded569b
TheBlurrypixel <mgochoco@yahoo.com>
2019-01-09 16:48:30 -0800

    refactored to recurse tokens via iterator

--- index.js
+++ index.js
@@ -10,53 +10,48 @@ var re2 = /^[^\:]*$/;
 function start() {
   var files = dialog.showOpenDialog({filters: [ {name: 'atlas', extensions: ['atlas']}, {name: 'All Files', extensions: ['*']} ] });
 
-  var res = {};
+  function assayTokens(inArr, inLevel) {
+    let res = {};
+    let i;
+    let reString = `^([\\s]{2}){${inLevel}}`;
+    let reLevel = new RegExp(reString);
 
-  function assayTokens(tokens) {
-    tokens.forEach((i) => {
-      if(re.test(i)) {
-        // console.log('i is: ' + i);
-        let solves = i.match(/[^\.]+/g);
-        res['name'] = solves.input ? solves : solves.reduce( (acc, cur) => {
-          return acc + '_' + cur;
-        });
+//    console.log(reString);
+
+    while((i = inArr.next().value) && reLevel.test(i)) {
+      // test if there is no colon if so then create a sub object
+      while(i && re2.test(i)) {
+        var outObj = assayTokens(inArr, inLevel+1);
+        res[i] = outObj.res;
+        i = outObj.nextName;
+        // console.log(i);
+//        res[outObj.nextName] = outObj.res;
       }
-      else if(re2.test(i)) {
-        console.log(i + ' going up a level');
-      }
-      else {
+
+      if(i) {
+        // there is a colon so first part is name for second part
         var propSplits = i.split(':', 2);
         var resSplits = propSplits.map( (item) => {
           return item.trim();
         });
         res[resSplits[0]] = resSplits[1];
       }
-    });
-  }
+    }
+
+    var retObj = {};
+    retObj.res = res;
+    retObj.nextName = i;
+
+    return retObj;
+}
 
 	if((files) && (files.length > 0)) {
-    var atlasText = fs.readFileSync(files[0], 'utf8');
-    var tokens = atlasText.trim().split('\n');
-    tokens.forEach((i) => {
-      if(re.test(i)) {
-        // console.log('i is: ' + i);
-        let solves = i.match(/[^\.]+/g);
-        res['name'] = solves.input ? solves : solves.reduce( (acc, cur) => {
-          return acc + '_' + cur;
-        });
-      }
-      else if(re2.test(i)) {
-        console.log(i + ' going up a level');
-      }
-      else {
-        var propSplits = i.split(':', 2);
-        var resSplits = propSplits.map( (item) => {
-          return item.trim();
-        });
-        res[resSplits[0]] = resSplits[1];
-      }
-    });
-    console.log(res);
+    let atlasText = fs.readFileSync(files[0], 'utf8');
+    let tokens = atlasText.trim().split('\n');
+
+    let eArr = tokens[Symbol.iterator]();
+    let res = assayTokens(eArr, 0);
+    console.log(res.res);
   }
   app.quit();
 }
