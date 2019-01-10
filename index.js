dedd4b725c0aa4fafcd26b6c4a965212f4694e9b
Michael Gochoco <mgochoco@yahoo.com>
2019-01-10 02:09:57 -0800

    added write JSON code

--- index.js
+++ index.js
@@ -16,16 +16,12 @@ function start() {
     let reString = `^([\\s]{2}){${inLevel}}`;
     let reLevel = new RegExp(reString);
 
-//    console.log(reString);
-
     while((i = inArr.next().value) && reLevel.test(i)) {
       // test if there is no colon if so then create a sub object
       while(i && re2.test(i)) {
         var outObj = assayTokens(inArr, inLevel+1);
-        res[i] = outObj.res;
+        res[i.replace(/\.+/g,'_').trim()] = outObj.res;
         i = outObj.nextName;
-        // console.log(i);
-//        res[outObj.nextName] = outObj.res;
       }
 
       if(i) {
@@ -34,7 +30,7 @@ function start() {
         var resSplits = propSplits.map( (item) => {
           return item.trim();
         });
-        res[resSplits[0]] = resSplits[1];
+        res[resSplits[0].replace(/\.+/g,'_')] = resSplits[1];
       }
     }
 
@@ -43,7 +39,7 @@ function start() {
     retObj.nextName = i;
 
     return retObj;
-}
+  }
 
 	if((files) && (files.length > 0)) {
     let atlasText = fs.readFileSync(files[0], 'utf8');
@@ -51,7 +47,11 @@ function start() {
 
     let eArr = tokens[Symbol.iterator]();
     let res = assayTokens(eArr, 0);
-    console.log(res.res);
+
+//    console.log(res.res);
+    var dirname = files[0].match(/(.*)[\/\\]/)[1]||'';
+    var filename = files[0].match(/(?:(?<=.*[\/\\]))([^\/\\]+\.atlas$)/)[1]||'';
+    fs.writeFileSync(dirname + ((process.platform !== 'darwin') ? '\\' : '\/') + filename.replace(/\.atlas$/, '_sheets.json'), JSON.stringify(res.res));
   }
   app.quit();
 }
